272eb21e66c68e7722b81f8220d4b807010e318a
Adri <adri.nemeth92@gmail.com>
2017-04-21 12:18:58 +0200

    Update and rename nav-buttons.component.js to nav-buttons.controller.js

--- nav-buttons.controller.js
+++ nav-buttons.controller.js
@@ -1,10 +1,6 @@
 // Register `navButtons` component, along with its associated controller and template
-angular.
-  module('navButtons').
-  component('navButtons', {
-    templateUrl: 'navbar/nav-buttons.template.html',
-    controller: function NavButtonsController() {
-      this.navbuttonItems = [
+portfolioApp.controller('NavButtonController', function NavButtonController($scope) {
+      $scope.navbuttonItems = [
         {
           btnText: 'About me',
         }, {
