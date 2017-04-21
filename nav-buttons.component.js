5a1e94f811003607c9bc35d4936326f59f6cb9e9
Adri <adri.nemeth92@gmail.com>
2017-04-21 12:01:25 +0200

    Create nav-buttons.component.js

---
+++ nav-buttons.component.js
@@ -0,0 +1,19 @@
+// Register `navButtons` component, along with its associated controller and template
+angular.
+  module('navButtons').
+  component('navButtons', {
+    templateUrl: 'navbar/nav-buttons.template.html',
+    controller: function NavButtonsController() {
+      this.navbuttonItems = [
+        {
+          btnText: 'About me',
+        }, {
+          btnText: 'About me',
+        }, {
+          btnText: ' PORTFOLIO',
+        }, {
+          btnText: ' CONTACT',
+        }
+      ];
+    }
+  });
